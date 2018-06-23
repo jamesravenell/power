9f2700db166c500ca0ccec5ffb77b652740d41e6
James Ravenell <james.ravenell@gmail.com>
2018-06-23 13:18:10 -0400

    Update Package

---
+++ powermodule/power.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import datetime
+
+dates = pd.date_range('20180101', periods=6)
--- powermodule/testnumpy.py
+++ powermodule/testnumpy.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python3
+import numpy as np
+import datetime
+start = datetime.datetime(2000, 1, 1)
+dt_array = np.array([start + datetime.timedelta(hours=i) for i in range(24)])
+print(dt_array)
